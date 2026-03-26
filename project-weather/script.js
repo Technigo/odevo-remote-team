d1d467610f70d39329b96a5b72d3a2037e86fc35
Lena Jonsson <lena.jonsson@odevo.com>
2026-03-26 11:57:14 +0100

    Set up API

--- project-weather/script.js
+++ project-weather/script.js
@@ -4,7 +4,21 @@ const description = document.getElementById("description")
 const icon = document.getElementById("icon")
 const errorMessage = document.getElementById("error")
 
+const apiKey = "1e4a62b378901f147a73ef2f8c6cc9c2"
+console.log(apiKey) 
+//const lat = 59.3293
+//const lon = 18.0686
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather"
+
 let city = "Stockholm"
+
+const url = `${baseUrl}?q=${city}&appid=${apiKey}`
+
+console.log(url)
+
+//https://api.openweathermap.org/data/3.0/weather?q=Stockholm,Sweden&units=metric&APPID=YOUR_API_KEY
+//http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 const fetchWeather = (city) => {
   // Write logic to fetch weather data from the API and display it on the page
